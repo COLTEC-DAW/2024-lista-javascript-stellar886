e8e6e2711c8a1207df3d2081b3d94fc02bbb5629
Stella Ventura <stellaventura88@gmail.com>
2024-06-12 10:16:05 -0300

    ex2

---
+++ ex2.js
@@ -0,0 +1,8 @@
+var num = prompt("quantas linhas?");
+for(let i = 0; i < num; i++){
+        if(i % 2 == 0){
+            console.log('# # # #');
+        }else{
+            console.log(' # # # #');
+        }
+}
