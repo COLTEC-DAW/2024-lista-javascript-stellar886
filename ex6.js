9509c4466b6277454cb8b8f80fb2e80520aca86e
Stella Ventura <stellaventura88@gmail.com>
2024-06-17 08:05:08 -0300

    ex6

--- ex6.js
+++ ex6.js
@@ -1,18 +1,24 @@
-
-var ehpar, ehmod;
 function mod2(number){
-    number % 2 == mod;
+    var ehpar;
     if(number % 2 == 0){
         ehpar = true;
     }else{
         ehpar = false;
     }
+  
+    return ehpar; 
 }
 
-function mod2(number, mod){
+function mod22(number, mod){
+    var ehmod;
     if(number % mod == 0){
         ehmod = true;
     }else{
         ehmod = false;
     }
+   
+    return ehmod; 
 }
+
+console.log(mod2(5)); 
+console.log(mod22(5, 2))
