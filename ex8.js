09f72793427b279fbdc0834732e638b3ec5763a5
Stella Ventura <stellaventura88@gmail.com>
2024-06-17 08:17:58 -0300

    8e9.2

--- ex8.js
+++ ex8.js
@@ -1,10 +1,14 @@
-var contador; 
+var frase = prompt("frase: ");
+var c = prompt("c: ");
 
-function countChars(frase, c){
-    for(let i = 0; i < frase.lenght; i++){
-        if(frase[0] == c){
+function countChars(frase, c) {
+    var contador = 0; // Inicializa o contador com 0
+    for (let i = 0; i < frase.length; i++) { // Corrige a propriedade length
+        if (frase[i] === c) { // Usa estritamente igual
             contador++;
         }
     }
-    return contador; 
+    return contador;
 }
+
+console.log(countChars(frase, c));
--- ex9.js
+++ ex9.js
@@ -1,9 +1,12 @@
-
+num1 = prompt ("menor num: ");
+num2 = prompt ("maior num: ");
 function range(min,max){
-    for(let i = min; i < max; i++){
+    for(let i = min; i <= max; i++){
         console.log(i);
     }
 }
+
+console.log(range(num1, num2));
 
 
 
