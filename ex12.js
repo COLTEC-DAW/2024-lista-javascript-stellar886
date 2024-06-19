42424c834c46c69639db4b57d1d35babd20db357
Stella Ventura <stellaventura88@gmail.com>
2024-06-19 08:50:53 -0300

    12.2

--- ex12.js
+++ ex12.js
@@ -6,9 +6,9 @@ function toList(array) {
     return list;
 }
 
-// Solicitar ao usuário para informar os elementos do array separados por vírgulas
-var userInput = prompt("Digite os elementos do array separados por vírgulas: ");
-var array = userInput.split(",").map(Number);  // Converte a string de entrada em um array de números
+
+var valores = prompt("Digite os elementos do array separados por vírgulas: ");
+var array = valores.split(",").map(Number);  // Converte a string de entrada em um array de números
 
 var list = toList(array);
 console.log(JSON.stringify(list, null, 2));  // Exibe a lista encadeada formatada no console
