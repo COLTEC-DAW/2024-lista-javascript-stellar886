c051a515b502beadcbd26d43026c309f7d652e4a
Stella Ventura <stellaventura88@gmail.com>
2024-06-19 09:02:33 -0300

    11.3

--- ex11.js
+++ ex11.js
@@ -1,8 +1,12 @@
-var arra = prompt("array: ");
-
-function reverse(arra) {
-    return arra.split("").reverse().join("");
+function reverseArray(str) {
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
 }
+let valores = prompt("Digite o array ");
 
-console.log(reverse(arra)); 
+let reversedArray = reverseArray(valores);
+console.log(reversedArray);
 
